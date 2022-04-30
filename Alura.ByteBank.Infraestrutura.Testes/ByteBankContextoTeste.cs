c1115a386a2241c6eecd39dd078dda6d08931314
Fabio Mendonca <fabio.jose.mendonca@gmail.com>
2022-04-29 21:43:42 -0300

    02 - Criando o primeiro teste

---
+++ Alura.ByteBank.Infraestrutura.Testes/ByteBankContextoTeste.cs
@@ -0,0 +1,34 @@
+﻿using Alura.ByteBank.Dados.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ByteBankContextoTeste
+    {
+        [Fact]
+        public void TestaConexaoComBDMySql()
+        {
+            //arrange
+            var contexto = new ByteBankContexto();
+            bool conectado;
+            //act
+
+            try
+            {
+                conectado = contexto.Database.CanConnect();
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("Não foi possivel conectar ao banco de dados!");
+            }
+            //assert
+            Assert.True(conectado);
+        }
+    }
+}
