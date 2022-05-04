cef2fa72965576d303a6b82927fae5200c6cae61
Fabio Mendonca <fabio.jose.mendonca@gmail.com>
2022-05-04 19:55:50 -0300

    Populando banco - sql

---
+++ Alura.ByteBank.Infraestrutura.Testes/ClienteRepositorioTeste.cs
@@ -0,0 +1,25 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{    
+    public class ClienteRepositorioTeste
+    {
+        [Fact]
+        public void TestaObterTodosClientes()
+        {
+            //arrange
+            var _respositorio = new ClienteRepositorio();
+            //act
+            List<Cliente> lista = _respositorio.ObterTodos();
+            //assert
+            Assert.NotNull(lista);
+        }
+    }
+}
