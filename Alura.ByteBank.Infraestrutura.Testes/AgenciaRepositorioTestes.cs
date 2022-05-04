a51e4856dcaed1ab22152f2c827cdbceb1c6d16c
Fabio Mendonca <fabio.jose.mendonca@gmail.com>
2022-05-04 20:21:07 -0300

    aula 06 - Testes Agencia

---
+++ Alura.ByteBank.Infraestrutura.Testes/AgenciaRepositorioTestes.cs
@@ -0,0 +1,38 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using Alura.ByteBank.Dominio.Interfaces.Repositorios;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class AgenciaRepositorioTestes
+    {
+        private readonly IAgenciaRepositorio _agendaRepositorio;
+
+        public AgenciaRepositorioTestes()
+        {
+            var servico = new ServiceCollection();
+            servico.AddTransient<IAgenciaRepositorio, AgenciaRepositorio>();
+
+            var provedor = servico.BuildServiceProvider();
+            _agendaRepositorio = provedor.GetService<IAgenciaRepositorio>();
+        }
+
+        [Fact]
+        public void TestaObterTodasAgencia()
+        {
+            //arrange
+            //act
+            List<Agencia> listaAgencia = _agendaRepositorio.ObterTodos();
+
+            //assert
+            Assert.NotNull(listaAgencia);
+        }
+    }
+}
